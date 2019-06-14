56faa5079986d69ca0dcef82dcf69798e2fdca91
Mohamed Saeed <msaeed027@gmail.com>
2019-06-14 23:43:52 +0200

    Updates

---
+++ precache-manifest.e87aba20e4a80762222280bcf57ecd47.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37d0df87a961bae187c737ce98312fb",
+    "url": "/reddit-react-redux/index.html"
+  },
+  {
+    "revision": "b4e3733ade3c703a892e",
+    "url": "/reddit-react-redux/static/js/2.27604685.chunk.js"
+  },
+  {
+    "revision": "24513276223195024aaf",
+    "url": "/reddit-react-redux/static/js/main.78b10a1d.chunk.js"
+  },
+  {
+    "revision": "da93a9d38c6d44136f1a",
+    "url": "/reddit-react-redux/static/js/runtime~main.2ad48622.js"
+  }
+]);
